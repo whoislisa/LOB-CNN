b26fb542d95b6b272094cdfaae13ef5c7be5cc82
liuyufei <liuyufei1106@foxmail.com>
2025-03-27 20:50:37 +0800

    traditional ML on non-imaged data

--- pipeline.py
+++ pipeline.py
@@ -10,8 +10,8 @@ import torch
 import utils as _U
 import model as _M
 
-code1 = '000002sz'
-folder_path = 'data_202101/'
+code1 = '600176sh'
+folder_path = 'data_202111/'
 df1 = pd.read_csv(folder_path + code1 + '.csv')
 
 # Chinese stock market trading time
@@ -25,20 +25,20 @@ df1.dropna(axis=0, inplace=True)
 df1.reset_index(drop=True, inplace=True)
 
 # traditional ML pipeline
-num_dataset = _U.generate_numerical_dataset(df1.iloc[:10000, :], record_cnt=5)  # full numerical dataset
+num_dataset = _U.generate_numerical_dataset(df1, record_cnt=5, pred_cnt=5)  # full numerical dataset
 labels = [entry[1] for entry in num_dataset]  # 只取ret5标签
 print(f"Label distribution: \n{pd.Series(labels).value_counts(normalize=True)}")
 
-df_results = _U.traditional_ml_pipeline(entries=num_dataset, balance=False, data_type='num')
+df_results = _U.traditional_ml_pipeline(dataset=num_dataset, balance=False, data_type='num')
 print(df_results.sort_values('F1', ascending=False))
 
-# deep learning pipeline
-image_dataset = _U.generate_dataset(df1.iloc[:10000, :], record_cnt=5)  # full image dataset
-labels = [entry[1] for entry in image_dataset]  # 只取ret5标签
-print(f"Label distribution: \n{pd.Series(labels).value_counts(normalize=True)}")
+# # deep learning pipeline
+# image_dataset = _U.generate_dataset(df1.iloc[:10000, :], record_cnt=5)  # full image dataset
+# labels = [entry[1] for entry in image_dataset]  # 只取ret5标签
+# print(f"Label distribution: \n{pd.Series(labels).value_counts(normalize=True)}")
 
-df_results = _U.traditional_ml_pipeline(entries=image_dataset, balance=False, data_type='img')
-print(df_results.sort_values('F1', ascending=False))
+# df_results = _U.traditional_ml_pipeline(dataset=image_dataset, balance=False, data_type='img')
+# print(df_results.sort_values('F1', ascending=False))
 
 # model = _M.BinaryCNN()
 # train_loader, val_loader, test_loader = _M.create_cnn_dataloaders(image_dataset, batch_size=16, target='ret5')
